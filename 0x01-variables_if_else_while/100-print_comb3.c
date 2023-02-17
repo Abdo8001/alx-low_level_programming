522463c682fe05f9fa5c29abda1583b7115c3b76
Abdo8001 <adosolim@gmail.com>
2023-02-17 04:41:31 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,34 +8,30 @@ int main(void)
 {
 	int i = '0';
 	int j = '0';
-	int k = '0';
 
-	while (i <= '7')
+	while (i <= '9')
 	{
-		while (j <= '8')
+		while (j <= '9')
 		{
-			while (k <= '9')
+			putchar(i);
+			putchar(j);
+			if (j == '9' && i == '9')
 			{
-				if (i < j && j < k)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(k);
-					if (!(i == '7' && j == '8' && k == '9'))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-
-				}
-				k++;
+				putchar('\n');
 			}
-			k = '0';
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
 			j++;
 		}
-		j = '0';
+		if (j >= '9')
+		{
+			j = '0';
+		}
 		i++;
 	}
-	putchar('\n');
 	return (0);
 }
+
